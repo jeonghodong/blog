392dd5a6554573ae2163e585126c9017d441cc6c
Hodong <fire13764@gmail.com>
2024-09-21 00:37:15 +0900

    chore: etc

--- app/(route)/post/[id]/page.tsx
+++ app/(route)/post/[id]/page.tsx
@@ -6,13 +6,15 @@ export default async function Page({ params }: { params: { id: string } }) {
   const decodedId = decodeURIComponent(id);
   const content = await getPostContent(id);
 
+  console.log(content);
+
   if (content === null) {
-    return <div className="container mx-auto p-4">포스트를 찾을 수 없습니다.</div>;
+    return <div>포스트를 찾을 수 없습니다.</div>;
   }
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-4">{decodedId.replace(/-/g, " ")}</h1>
+    <div>
+      <h1>{decodedId.replace(/-/g, " ")}</h1>
       <ReactMarkdown>{content}</ReactMarkdown>
     </div>
   );
